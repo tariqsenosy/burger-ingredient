636902127320c0fea3cf9666aacd4d7f405f761c
tariqsenosy <tariqsenosy@gmail.com>
2023-12-08 15:58:44 +0200

    After Deadline Commit : Enhance ingredient deduction logic for only one query  && avoid nth ingredient not enough and need for rollback

--- burger-app/app/Services/OrderService.php
+++ burger-app/app/Services/OrderService.php
@@ -8,6 +8,7 @@ use App\Repositories\IngredientWarningRepository;
 use App\Repositories\ProductRepository;
 use App\Models\Ingredient;
 use App\Events\LowStockWarningEvent;
+use DB;
 
 
 class OrderService
@@ -21,42 +22,53 @@ class OrderService
 
     public function placeOrder(array $orderData)
     {
-        try{
-        // Get products with ingredients from the payload
-        $products = $this->productRepository->getProductsFromPayload($orderData);
-
-        // Additional logic to check and deduct stock from ingredients
-        foreach ($products as $product) {
-            foreach ($product->ingredients as $ingredient) {
-                //get quantity of current product [ex: 2 Burger]
-                $numberOfUnitsWithIngredient = collect($orderData['products'])
-                    ->where('product_id', $product->id)
-                    ->first()['quantity'];
-                // Check if there's enough stock
-                //- multiply number of Burger * quantity of cheese needed 
-                $quantityToBeDeducted= $numberOfUnitsWithIngredient *  $ingredient->pivot->quantity;
-                if ($ingredient->stock >= $quantityToBeDeducted) {
-                    // Deduct stock
-                    $ingredient->stock -= $quantityToBeDeducted;
-                    $ingredient->save();
-                    // Check and send warning/notification if percentage match
-                    $this->checkIngredientStock($ingredient);
-                } else {
-                    // Handle insufficient stock scenario
-                    throw new \Exception('Insufficient stock for ingredient: ' . $ingredient->name);
+        try {
+            DB::transaction(function () use ($orderData) {
+                // Get products with ingredients from the payload
+                $products = $this->productRepository->getProductsFromPayload($orderData);
+    
+                // Build a single query for stock updates using a CASE statement
+                //Avoid case of not sufficient in nth ingredient and need rollback
+                $caseStatements = [];
+    
+                // Additional logic to check and deduct stock from ingredients
+                foreach ($products as $product) {
+                    foreach ($product->ingredients as $ingredient) {
+                        // Get quantity of the current product
+                        $numberOfUnitsWithIngredient = collect($orderData['products'])
+                            ->where('product_id', $product->id)
+                            ->first()['quantity'];
+    
+                        // Check if there's enough stock
+                        $quantityToBeDeducted = $numberOfUnitsWithIngredient * $ingredient->pivot->quantity;
+    
+                        if ($ingredient->stock >= $quantityToBeDeducted) {
+                            // Build a CASE statement for each ingredient
+                            $caseStatements[] = "WHEN id = {$ingredient->id} THEN stock - $quantityToBeDeducted";
+    
+                            // Check and send warning/notification if percentage match
+                            $this->checkIngredientStock($ingredient);
+                        } else {
+                            // Handle insufficient stock scenario
+                            throw new \Exception('Insufficient stock for ingredient: ' . $ingredient->name);
+                        }
+                    }
                 }
-            }
-        }
-            // Create the order
+    
+                // one query update 
+                $caseStatements = implode(' ', $caseStatements);
+                $query = "UPDATE ingredients SET stock = CASE $caseStatements END";
+                DB::statement($query);
+            });
+    
+            // Create the order after successful stock updates
             return $this->orderRepository->createOrder($orderData);
-        }catch(\Exception $e)
-        {
+        } catch (\Exception $e) {
             // Return a JSON response with the error message
             return response()->json(['error' => $e->getMessage()], 400);
         }
-        
     }
-
+    
     protected function checkIngredientStock(Ingredient $ingredient)
     {
         //get percentage of current stock over last stock charge 
